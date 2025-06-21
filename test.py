db2888a96f1efdd4ca1a7299ea6afcfdfb1a4958
Rohan Narigara <rohannarigara22@gmail.com>
2025-06-21 22:57:44 +0200

    Chapter 10 Completed

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-import random
+class Employee:
+    language = "Python"
+    salary = 1200000
 
-choice = random.choice([-1, 0, 1])
-print(choice)
+    def __init__(self, language, salary):
+        self.language = language
+        self.salary = salary
+        print("I am running")
+
+harry = Employee("JavaScript", 1300000)
+print(harry.language, harry.salary)
